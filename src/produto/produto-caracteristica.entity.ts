0f3d6111d0547e1dd31d107bcec6eec997584ce8
Douglas Viegas <54741486+douglasdmv@users.noreply.github.com>
2023-07-11 21:45:33 -0300

    feat: criação das entidades

---
+++ src/produto/produto-caracteristica.entity.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'produto_caracteristicas' })
+export class ProdutoCaracteristica {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'nome', length: 100, nullable: false })
+  nome: string;
+
+  @Column({ name: 'descricao', length: 255, nullable: false })
+  descricao: string;
+}
---
+++ src/produto/produto-imagem.entity.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'produto_imagens' })
+export class ProdutoImagem {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'url', length: 100, nullable: false })
+  url: string;
+
+  @Column({ name: 'descricao', length: 255, nullable: false })
+  descricao: string;
+}
